63fe1cc3831c555f5157188cbac6eb8f995425bb
ustaaaaaalll@gmail.com <ustaaaaaalll@gmail.com>
2025-05-19 20:59:29 +0300

    Добавьте файлы проекта.

---
+++ Bot/Program.cs
@@ -0,0 +1,48 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Bot
+{
+    internal class Program
+    {
+        private static void UpdateReceived(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            if (update.Message?.From is null)
+            {
+                Console.WriteLine("Message is null");
+                return;
+            }
+            var info = $"{update.Message.From.Username} : {update.Message.Text}";
+            Console.WriteLine(info);
+            botClient.SendMessage(update.Message.Chat.Id, info, cancellationToken: token);
+        }
+
+        static async Task Main()
+        {
+            var cts = new CancellationTokenSource();
+            string? key = Environment.GetEnvironmentVariable("BOT_TOKEN_HW");
+
+            var botClient = new TelegramBotClient(key);
+
+            var me = await botClient.GetMe(cts.Token);
+            Console.WriteLine($"Hello, {me.FirstName} {me.LastName} with username {me.Username} and id {me.Id}!");
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = [UpdateType.Message],
+                DropPendingUpdates = true
+            };
+            var handler = new UpdateHandler();
+            botClient.StartReceiving(handler, receiverOptions);
+
+            //botClient.StartReceiving(UpdateReceived, (_, exception, _) => Console.WriteLine(exception.Message),
+            //new ReceiverOptions()
+            //{
+            //    AllowedUpdates = [UpdateType.Message]
+            //}, cts.Token);
+            Console.ReadKey();
+            await cts.CancelAsync();
+        }
+    }
+}
---
+++ Bot/UpdateHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+namespace Bot
+{
+    class UpdateHandler : IUpdateHandler
+    {
+        public Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, HandleErrorSource source, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            var info = "Сообщение получено!";
+            Console.WriteLine(info);
+            botClient.SendMessage(update.Message?.Chat.Id, info, cancellationToken: token);
+
+           // Console.WriteLine("Сообщение получено!");
+            return Task.CompletedTask;
+        }
+    }
+}
